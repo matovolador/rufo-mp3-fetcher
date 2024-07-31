f06063ce9fe4144c6cd67557506823e94723e653
Matias Garafoni <matias.garafoni@gmail.com>
2024-07-30 23:16:05 -0300

    fixed package dependencies

--- install-extras.sh
+++ install-extras.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # you will have to execute this script as sudo
 echo "installing ffmpeg"
-apt install ffmpeg
+apt install ffmpeg -y
+echo "installing tkinter"
+apt install python3-tk -y
 echo "Installing python3-dev"
 apt install python3-dev -y
 echo "Installing virtualenv"
-apt-get install virtualenv
+apt-get install virtualenv -y
